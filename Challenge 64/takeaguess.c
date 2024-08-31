000159705ef35ecd885ed076b0840a40b59c2067
J Melg <rjmzelaya@gmail.com>
2024-08-30 20:53:21 -0600

    Game takes into consideration wether user inputs anumber out of range

--- Challenge 64/takeaguess.c
+++ Challenge 64/takeaguess.c
@@ -21,37 +21,59 @@ int main(){
 //	it will always output the same "random int"
 	srand((unsigned int) time(&t));
 	
-	int answ = rand() % 21;
+	int answ; 
 		
 //	printf("%d", answ);
-	int guess = 1;
-	printf("Get ready to guess the number!\n\n");
-	int attempts = 1;
+	int guess;
+	
+	int attempts;
+	
+	char cont = 'y';
+	
+	while (cont == 'y') {
+		guess =1; attempts =1;
+		
+		answ = rand() % 21;//gets a new number with every game
+		
+		printf("\nGet ready to guess the number!\nGuess the number between 0 and 20\n\n");
+		
+		for(;attempts <=5; ++attempts) {
+			//best way to print plural or singular i love it
+			//printf("You have %d tr%s left", numberOfGuesses, numberOfGuess == 1 ? "y" : "ies");
+			printf("This is your attempt #%d\n", attempts);
+			printf("Your guess: ");
+			scanf("%d", &guess);
+			
+			//to help with guiding guess or if it's right
+			if(guess == answ) {
+				printf("Correct! That's the right answer\n");
+				break;
+			}
+			else if(guess > answ) {
+				printf("Sorry, your answer is not correct\n");
+				printf("Your guess is actually too high\n\n");
+			}
+			else if(guess < answ) {
+				printf("Sorry, your answer is not correct\n");
+				printf("Your guess is actually too low\n\n");
+			}
+			
+			//if more than 20 or less than 0
+			if (guess < 0 || guess > 20) {
+				printf("Sorry, but the number has to be between 0 and 20\n\n");
+				--attempts;//try again
+			}
+			
+		}
+		//if not right answer
+		if(attempts ==6)
+			printf("So Sorry, the right answer was actually %d\n", answ);
+		
+		printf("\nWant to play again? y/n: ");
+		scanf(" %c", &cont);	
 
-
-	while(attempts <=5 && guess > 0 && guess <= 20) {
-		printf("This is your attempt #%d\n", attempts);
-		printf("Your guess: ");
-		scanf("%d", &guess);
-		if(guess == answ) {
-			printf("Correct! That's the right answer\n");
-			break;
-		}
-		else if(guess > answ) {
-			printf("Sorry, your answer is not correct\n");
-			printf("Your guess is actually too high\n\n");
-			attempts++;
-		}
-		else if(guess < answ) {
-			printf("Sorry, your answer is not correct\n");
-			printf("Your guess is actually too low\n\n");
-			attempts++;
-		}
-		if(attemps ==5)
-			printf("So Sorry, the right answer was actually %d", answ);
 	}
-
-
+	printf("\nGoodbye! :D\n");
 	return 0;
 
 }
