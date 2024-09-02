89c812a3d3161ef70b8e31e4958c31e1298d4ac6
J Melg <rjmzelaya@gmail.com>
2024-09-01 21:56:42 -0600

    Challenge 79, practice with functions

---
+++ Challenge 79/functions.c
@@ -0,0 +1,30 @@
+//this challenge has you write 3 functs
+
+//I. a func that finds the greatest commond divisor of two non negative ints and return the result GCD or MCD
+//II. Write a funct to calculate the absolute val of a number. takes a float
+//III. compute the square root of a number and should use func II
+//if negative is input it sould return -1.0
+
+#include <stdio.h>
+#include <stdbool.h>
+
+//Function 1 maximo comun divisor
+int findMCD(int a, int b) {
+	int common = 1;
+	int divisor = 1;
+	//divides through
+	bool found = 0;
+	while(found == 0) {
+		if((a % divisor == 0) && (b % divisor == 0)) {
+		
+		}
+		else {
+			
+		}
+	}
+}
+
+int main() { 
+    printf("hello");
+    return 0;
+}
