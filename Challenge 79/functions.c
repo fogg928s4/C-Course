2fc0ac5dc263e940585079cf0a3aa47964305211
J Melg <rjmzelaya@gmail.com>
2024-09-02 09:56:48 -0600

    Added a menu, and the absolute value function. GCD works...partially

--- Challenge 79/functions.c
+++ Challenge 79/functions.c
@@ -6,25 +6,87 @@
 //if negative is input it sould return -1.0
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-//Function 1 maximo comun divisor
-int findMCD(int a, int b) {
-	int common = 1;
-	int divisor = 1;
-	//divides through
-	bool found = 0;
-	while(found == 0) {
-		if((a % divisor == 0) && (b % divisor == 0)) {
-		
-		}
-		else {
-			
-		}
+//function headers
+
+void menu();
+int findMCD(int a, int b);
+float absVal(float number);
+
+int main() { 
+	menu();
+    return 0;
+}
+
+void menu() {
+	int choice = 0;
+	float num0,num1;
+	
+	printf("Functions practice: Enter the number of function to test\n");
+	printf("0. Find the GCD/MCD of two numbers\n");
+	printf("1. Find the Absolute Value of a number\n");
+	printf("2. Find the square root of a number\n");
+	printf("+++ Your choice: ");
+	scanf("%d", &choice);
+	switch(choice) {
+		//case for mcd
+		case 0:
+			printf("\nInput the first number: ");
+			scanf("%f", &num0);
+			printf("Input the second number: ");
+			scanf("%f", &num1);
+			printf("\nThe mcd is %d\n",findMCD(num0, num1));
+			break;
+		//case for absolute
+		case 1:	
+			printf("\nInput the number to find the absolute of: ");
+			scanf("%f", &num0);
+			printf("The |%0.2f| is %0.2f\n", num0, absVal(num0));
+			break;
+		default:
+			printf("\nNot a valid option!\n\n");
+			menu();
+			break;
 	}
 }
 
-int main() { 
-    printf("hello");
-    return 0;
+int findMCD(int a, int b) {
+	//determine the shorter number
+	int divisor = 2;
+	int mcd = 1;
+	
+	printf("a=%d\tb=%d\n",a,b);
+	
+	//loop to find the mcd
+	while((divisor <= a) || (divisor <= b)) {
+		
+		if ((a % divisor == 0) && (b % divisor == 0)) {
+			printf("a=%d\tb=%d\t| %d\n",a,b,divisor);
+			a /= divisor; b /= divisor; 
+			mcd *= divisor;
+			
+		}
+		else if ((a % divisor != 0) && (b%divisor == 0)) {
+			printf("a=%d\tb=%d\t| %d\n",a,b,divisor);
+			b /= divisor;
+			return mcd;
+		}	
+		else if ((a % divisor == 0) && (b%divisor != 0)) {
+			printf("a=%d\tb=%d\t| %d\n",a,b,divisor);
+			a /= divisor;
+			return mcd;
+		}
+		else
+			divisor++;
+	}
+	return mcd;
+}
+
+float absVal(float number) {
+	if(number < 0)
+		return number * -1;
+	else 
+		return number;
 }
